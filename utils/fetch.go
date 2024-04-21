a569a00fbbf60c6076f648c4307e2f5820774cac
Sean <107402787+Pythonwithsean@users.noreply.github.com>
2024-04-21 23:15:00 +0100

    "Image Bounds"

--- utils/fetch.go
+++ utils/fetch.go
@@ -1,12 +1,10 @@
 package utils
 
 import (
-	"fmt"
 	"image"
 	_ "image/png"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
@@ -23,26 +21,33 @@ func FetchImage(addr string) (string, error) {
 	// }
 	
 	// body,err := ioutil.ReadFile(addr)
-	reader, err := os.Open(string(strings.TrimSpace(addr)))
+	file, err := os.Open(string(strings.TrimSpace(addr)))
 	if err != nil {
 		log.Fatal("Error Opening File", err)
 	}
 
 	// Close Reader When Function gets popped off Call stack
-	defer reader.Close()
+	defer file.Close()
 
-	switch ext := filepath.Ext(string(reader.Name()));  ext {
-	case ".jpeg":
-		fmt.Println("Your File is a JPEG file")
-	case ".png":
-		fmt.Println("Your File is a PNG file")
-	}
-
-	imgData,_,err := image.Decode(reader)
+	// switch ext := filepath.Ext(string(reader.Name()));  ext {
+	// case ".jpeg":
+	// 	fmt.Println("Your File is a JPEG file")
+	// case ".png":
+	// 	fmt.Println("Your File is a PNG file")
+	// }
+	/* Decoded image  after opening reader  */
+	imgData,_,err := image.Decode(file)
 	if err != nil { 
 		log.Fatal(err)
 	}
-	fmt.Println(imgData)
+	bounds := imgData.Bounds()
+	for y := bounds.Min.Y;  y< bounds.Max.Y; y++{ 
+		for x := bounds.Min.X; x < bounds.Max.X; x++{ 
+			r,g,b,a := imgData.At(x,y).RGBA()
+
+
+		}
+	} 
 	
 	
 	return "", nil
