213cb134b01d12907973458d79a22e923aaba03b
Sean <107402787+Pythonwithsean@users.noreply.github.com>
2024-04-21 23:46:47 +0100

    "Commit"

--- utils/fetch.go
+++ utils/fetch.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"image"
 	_ "image/png"
 	"log"
@@ -40,20 +41,32 @@ func FetchImage(addr string) (string, error) {
 		I have Chosen to use the Luminance formula grayscale = 0.2126 * Red + 0.7152 * Green + 0.00722 * Blue this formula will take into account
 		the humans eyes sensitivity to different colors and provies a better representation of brighness 
 		*/
+		//Luminance (perceived option 1): (0.299*R + 0.587*G + 0.114*B)
 
 		//Ascii symbols
+	brightAscii := " "
+	darkAscii := "#"
 		
 	/* Decoded image  after opening reader  */
 	imgData,_,err := image.Decode(file)
 	if err != nil { 
 		log.Fatal(err)
 	}
+
+	// Iterate Over bounds 
 	bounds := imgData.Bounds()
-	for y := bounds.Min.Y;  y< bounds.Max.Y; y++{ 
-		for x := bounds.Min.X; x < bounds.Max.X; x++{ 
-			r,g,b,a := imgData.At(x,y).RGBA()
+	for x := bounds.Min.X; x < bounds.Max.X; x++{ 
+		for y := bounds.Min.Y;  y< bounds.Max.Y; y++{ 
+			r,g,b,_ := imgData.At(x,y).RGBA()
+			brigthness := (0.299* float64(r) + 0.587* float64(g) + 0.114* float64(b))
+			bright :=  brigthness >=  150 
+			dark  := brigthness <= 100  
+			if bright {
+				fmt.Print(brightAscii)
 
-
+			}else if dark {
+				fmt.Print(darkAscii)
+			}
 		}
 	} 
 	
